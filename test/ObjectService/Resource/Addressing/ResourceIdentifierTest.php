118aa76ede189f08f82aaa2a85917c6992b46b75
Piotr Szymanski <szyman@magres.net>
2014-10-28 21:00:30 +0100

    Improve test

--- test/ObjectService/Resource/Addressing/ResourceIdentifierTest.php
+++ test/ObjectService/Resource/Addressing/ResourceIdentifierTest.php
@@ -15,24 +15,44 @@ class ResourceIdentifierTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals($url, $resourceIdentifier->getUrl());
 	}
 	
-	public function testResolution()
+	public function testResolutionOfRootObject()
 	{
 		$registry = $this->createEndpointRegistry();
-		
+
 		$url = "http://example.org/endpoint/resource";
 		$resourceIdentifier = ResourceIdentifier::createFromUrl($url);
 		$resolved = $resourceIdentifier->resolve($registry);
 
 		$this->assertEquals("", $resolved->getPath());
 		$this->assertEquals(0, count($resolved->getElements()));
-		$this->assertEquals("resource", $resolved->getSourceResource()->getEndpointUrl()->getRelativeUrl());
+
+		$baseResource = $resolved->getSourceResource();
+		$this->assertEquals("resource", $baseResource->getEndpointUrl()->getRelativeUrl());
+		$this->assertEquals("Title", $baseResource->getValue()->title);
+		$this->assertInstanceOf('Light\ObjectService\Mockup\PostModel', $baseResource->getType());
 	}
-	
+
+	public function testResolutionOfRootObjectProperty()
+	{
+		$registry = $this->createEndpointRegistry();
+
+		$url = "http://example.org/endpoint/resource/title";
+		$resourceIdentifier = ResourceIdentifier::createFromUrl($url);
+		$resolved = $resourceIdentifier->resolve($registry);
+
+		$this->assertEquals("title", $resolved->getPath());
+		$this->assertEquals("title", $resolved->getLastElement());
+		$this->assertEquals(1, count($resolved->getElements()));
+
+		$baseResource = $resolved->getSourceResource();
+		$this->assertEquals("resource", $baseResource->getEndpointUrl()->getRelativeUrl());
+	}
+
 	protected function createEndpointRegistry()
 	{
 		$registry = new EndpointRegistry();
 		$endpoint = Endpoint::create("http://example.org/endpoint");
-		$endpoint->getObjectRegistry()->publishObject("resource", new Post(), new PostModel());
+		$endpoint->getObjectRegistry()->publishObject("resource", new Post(10, "Title"), new PostModel());
 		$registry->addEndpoint($endpoint);
 
 		return $registry;
