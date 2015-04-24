a0fbfd3ad7e4a354836fbfecc2b8b62525c60614
Piotr Szymanski <szyman@magres.net>
2015-04-24 16:15:06 +0200

    Test read and execute of append operation

--- test/ObjectService/Json/Request/AppendOperationReaderTest.php
+++ test/ObjectService/Json/Request/AppendOperationReaderTest.php
@@ -23,4 +23,29 @@ class AppendOperationReaderTest extends \PHPUnit_Framework_TestCase
 		$resource = $operation->getElementResource();
 		$this->assertInstanceOf(ResolvedObject::class, $resource);
 	}
+
+	public function testReadAndExecute()
+	{
+		$setup = Setup::create();
+
+		$count = count($setup->getDatabase()->getPosts());
+
+		$subjectResource = $setup->getEndpointRegistry()->getResource("http://example.org/collections/post");
+
+		$json = new \stdClass;
+		$json->meta = new \stdClass;
+		$json->meta->spec = "new";
+		$json->meta->type = "php:" . Post::class;
+
+		$json->data = new \stdClass();
+		$json->data->title = "My new post";
+
+		$reader = new AppendOperationReader($setup->getExecutionParameters());
+		$operation = $reader->read($json);
+
+		$operation->execute($subjectResource, $setup->getExecutionParameters());
+
+		// Check that the element was appended
+		$this->assertEquals($count+1, count($setup->getDatabase()->getPosts()));
+	}
 }
