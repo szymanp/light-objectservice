7608594da73a94d750003baaed62f2684693c993
Piotr Szymanski <szyman@magres.net>
2016-03-31 21:23:58 +0200

    Start on DefaultRestRequestReaderConfiguration

--- src2/Configuration/RestRequestReaderConfiguration.php
+++ src2/Configuration/RestRequestReaderConfiguration.php
@@ -1,5 +1,8 @@
 <?php
 namespace Szyman\ObjectService\Configuration;
+use Szyman\ObjectService\Service\RequestBodyDeserializerFactory;
+use Szyman\ObjectService\Service\RequestHandlerFactory;
+use Szyman\ObjectService\Service\ResponseCreatorFactory;
 
 /**
  * Configuration for the RestRequestReader.
@@ -11,10 +14,22 @@ interface RestRequestReaderConfiguration
 	 * @return RequestBodyTypeMap
 	 */
 	public function getRequestBodyTypeMap();
-	
+
+	/**
+	 * Returns a RequestBodyDeserializerFactory used for instantiating deserializers.
+	 * @return RequestBodyDeserializerFactory
+	 */
 	public function getRequestBodyDeserializerFactory();
-	
+
+	/**
+	 * Returns a RequestHandlerFactory.
+	 * @return RequestHandlerFactory
+	 */
 	public function getRequestHandlerFactory();
-	
+
+	/**
+	 * Returns a ResponseCreatorFactory for instantiating response creators.
+	 * @return ResponseCreatorFactory
+	 */
 	public function getResponseCreatorFactory();
 }
