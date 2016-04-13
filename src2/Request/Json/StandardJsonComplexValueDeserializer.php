11e10bf1d7d9c2514f5a24cd226d885ebccad141
Piotr Szymanski <szyman@magres.net>
2016-04-13 17:00:23 +0200

    Deserializer - work in progress.

--- src2/Request/Json/StandardJsonComplexValueDeserializer.php
+++ src2/Request/Json/StandardJsonComplexValueDeserializer.php
@@ -1,7 +1,9 @@
 <?php
 namespace Szyman\ObjectService\Request\Json;
 
-use Light\ObjectAccess\Type\ComplexType;
+use Light\ObjectAccess\Type\ComplexTypeHelper;
+use Light\ObjectService\Exception\MalformedRequest;
+use Szyman\ObjectService\Resource\KeyValueComplexValueRepresentation;
 use Szyman\ObjectService\Service\ComplexValueModification;
 use Szyman\ObjectService\Service\ComplexValueModificationDeserializer;
 use Szyman\ObjectService\Service\ComplexValueRepresentation;
@@ -9,45 +11,78 @@ use Szyman\ObjectService\Service\ComplexValueRepresentationDeserializer;
 
 class StandardJsonComplexValueDeserializer implements ComplexValueRepresentationDeserializer, ComplexValueModificationDeserializer
 {
+	const REPLACE = 1;
+	const UPDATE = 2;
+
 	// TODO: Maybe it would be better to implement this class as an abstract class with two concrete ones.
 
-	/** @var ComplexType */
-	private $type;
+	/** @var ComplexTypeHelper */
+	private $typeHelper;
 
-	private $replace;
+	private $mode;
 
-	protected function __construct(ComplexType $type, $replace = false)
+	protected function __construct(ComplexTypeHelper $typeHelper, $mode)
 	{
 		// TODO
 	}
 
 	/**
 	 * Creates a new deserializer for a full object representation.
-	 * @param ComplexType $type
+	 * @param ComplexTypeHelper $typeHelper
 	 * @return StandardJsonComplexValueDeserializer
 	 */
-	public static function newRepresentationDeserializer(ComplexType $type)
+	public static function newRepresentationDeserializer(ComplexTypeHelper $typeHelper)
 	{
-		return new self($type, true);
+		return new self($typeHelper, self::REPLACE);
 	}
 
 	/**
 	 * Creates a new deserializer for a partial object representation.
-	 * @param ComplexType $type
+	 * @param ComplexTypeHelper $typeHelper
 	 * @return StandardJsonComplexValueDeserializer
 	 */
-	public static function newModificationDeserializer(ComplexType $type)
+	public static function newModificationDeserializer(ComplexTypeHelper $typeHelper)
 	{
-		return new self($type, false);
+		return new self($typeHelper, self::UPDATE);
 	}
 
 	/**
 	 * Deserializes the object.
 	 * @param string $content
 	 * @return ComplexValueRepresentation|ComplexValueModification
+	 * @throws MalformedRequest	Thrown if the content does not match the expected format.
 	 */
 	public function deserialize($content)
 	{
-		// TODO
+		if (is_resource($content))
+		{
+			$content = stream_get_contents($content);
+			if ($content === false) throw new \RuntimeException("Could not read from stream");
+		}
+		
+		$json = json_decode($content)
+		if (is_null($json))
+		{
+			throw new MalformedRequest("Could not convert request body to JSON");
+		}
+		
+		
+		$result = $this->readJson($json);
+		
+		return $result;
+	}
+	
+	private function readJson($json)
+	{
+		$result = new KeyValueComplexValueRepresentation();
+
+		foreach($json as $fieldName => $fieldValue)
+		{
+			if (is_scalar($fieldValue))
+			{
+				$result->setValue($fieldName, $fieldValue);
+			}
+			// TODO
+		}
 	}
 }
