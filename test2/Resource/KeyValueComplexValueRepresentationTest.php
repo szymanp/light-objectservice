3d1e0e7c0c3364a0af4b494188af8432b2182dae
Piotr Szymanski <szyman@magres.net>
2016-04-15 14:29:13 +0200

    Test for KeyValueComplexValueRepresentation

---
+++ test2/Resource/KeyValueComplexValueRepresentationTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace Szyman\ObjectService\Resource;
+
+use Light\ObjectAccess\Resource\Origin;
+use Light\ObjectAccess\Resource\ResolvedObject;
+use Light\ObjectAccess\Resource\Util\EmptyResourceAddress;
+use Light\ObjectService\Resource\Addressing\UrlUnresolvedAddress;
+use Light\ObjectService\TestData\Post;
+use Light\ObjectService\TestData\Setup;
+
+class KeyValueComplexValueRepresentationTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var Setup */
+	private $setup;
+
+	protected function setUp()
+	{
+		parent::setUp();
+		$this->setup = Setup::create();
+	}
+
+	public function testUpdateScalarFields()
+	{
+		$rep = new KeyValueComplexValueRepresentation();
+		$rep->setValue('id', 123456);
+		$rep->setValue('title', 'A test value');
+
+		$post = new Post();
+		$resource = $this->newResource($post);
+		$rep->updateObject($resource, $this->setup->getExecutionParameters());
+
+		$this->assertEquals(123456, 		$post->getId());
+		$this->assertEquals('A test value', $post->getTitle());
+	}
+
+	public function testUpdateReferenceFields()
+	{
+		$rep = new KeyValueComplexValueRepresentation();
+		$rep->setResource('author', new ExistingResourceReference(new UrlUnresolvedAddress('http://example.org/resources/max')));
+
+		$post = new Post();
+		$resource = $this->newResource($post);
+		$rep->updateObject($resource, $this->setup->getExecutionParameters());
+
+		$this->assertSame($this->setup->getDatabase()->getAuthor(1010), $post->getAuthor());
+	}
+
+	/**
+	 * @param object $object
+	 * @return ResolvedObject
+	 */
+	private function newResource($object)
+	{
+		return new ResolvedObject($this->setup->getTypeRegistry()->getComplexTypeHelper(get_class($object)), $object, EmptyResourceAddress::create(), Origin::unavailable());
+	}
+}
